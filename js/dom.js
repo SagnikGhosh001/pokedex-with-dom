5fa69b11144996a81648f88051b86ce60cf0e6a2
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Pro.local>
2026-03-05 13:46:09 +0530

    abstraction

--- js/dom.js
+++ js/dom.js
@@ -6,73 +6,80 @@ export const createLoader = () => {
   return loader;
 };
 
-export const createElementTemplate = (tag, attributes, contents) => ({
-  tag,
-  attributes,
-  contents,
-});
-
-const createPokemonName = (name) =>
-  createElementTemplate("div", [["class", "name"]], [
-    createElementTemplate("h1", [["class", "name"]], capitalize(name)),
-  ]);
+const createPokemonName = (
+  name,
+) => [
+  "div",
+  { "class": "name" },
+  ["h1", { "class": "name" }, name],
+];
 
 const createTypeBadges = (types) =>
   types.map((type) => {
-    const p = createElementTemplate(
+    const p = [
       "p",
-      [["class", `type ${type.type.name}`]],
-      capitalize(type.type.name),
-    );
+      { "class": `type ${type.type.name}` },
+      type.type.name,
+    ];
 
     return p;
   });
 
-const createTypeBadgeContainer = (types) =>
-  createElementTemplate("div", [["class", "types"]], createTypeBadges(types));
+const createTypeBadgeContainer = (
+  types,
+) => ["div", { "class": "types" }, ...createTypeBadges(types)];
 
-const createCardHeader = (name, types) =>
-  createElementTemplate("div", [["class", "header"]], [
-    createPokemonName(name),
-    createTypeBadgeContainer(types),
-  ]);
+const createCardHeader = (
+  name,
+  types,
+) => [
+  "div",
+  { "class": "header" },
+  createPokemonName(name),
+  createTypeBadgeContainer(types),
+];
 
 const createStatRows = (stats) =>
-  stats.map(({ stat, base_stat }) => {
-    const div = createElementTemplate("div", [["class", "name-power"]], [
-      createElementTemplate("p", [["class", "name"]], capitalize(stat.name)),
-      createElementTemplate("p", [["class", "power"]], base_stat),
-    ]);
-
-    return div;
-  });
-
-const createStatsSection = (stats) =>
-  createElementTemplate("div", [["class", "stats"]], createStatRows(stats));
-
-const createCardMetadata = (name, types, stats) =>
-  createElementTemplate("div", [["class", "meta-data"]], [
-    createCardHeader(name, types),
-    createStatsSection(stats),
+  stats.map((
+    { stat, base_stat },
+  ) => [
+    "div",
+    { "class": "name-power" },
+    ["p", { "class": "name" }, stat.name],
+    ["p", { "class": "power" }, base_stat],
   ]);
 
+const createStatsSection = (
+  stats,
+) => ["div", { "class": "stats" }, ...createStatRows(stats)];
+
+const createCardMetadata = (name, types, stats) => [
+  "div",
+  { "class": "meta-data" },
+  createCardHeader(name, types),
+  createStatsSection(stats),
+];
+
 const createImageSection = (pokeUrl, name, types) => {
-  const attributes = [["class", "img-container"], [
-    "style",
-    `background-image: linear-gradient(${createImageGradient(types)},white)`,
-  ]];
+  const attributes = {
+    "class": "img-container",
+    "style": `background-image: linear-gradient(${
+      createImageGradient(types)
+    },white)`,
+  };
 
-  const imageTag = createElementTemplate("img", [["src", pokeUrl], [
-    "alt",
-    `${name} image`,
-  ], ["class", "poke-image"]], "");
+  const imageTag = ["img", {
+    "src": pokeUrl,
+    "alt": `${name} image`,
+    "class": "poke-image",
+  }, ""];
 
-  return createElementTemplate("div", attributes, [imageTag]);
+  return ["div", attributes, imageTag];
 };
 
 const createPokemonCardTemplates = (pokemon) =>
   pokemon.map(({ sprites, name, types, stats }) => {
-    const card = createElementTemplate("div", [["class", "card"]], []);
+    const card = ["div", { "class": "card" }];
 
     const imageContainer = createImageSection(
       sprites.other["official-artwork"].front_default,
@@ -80,34 +87,34 @@ const createPokemonCardTemplates = (pokemon) =>
       types,
     );
     const metaDataContainer = createCardMetadata(name, types, stats);
-    card.contents.push(imageContainer, metaDataContainer);
+    card.push(imageContainer, metaDataContainer);
 
     return card;
   });
 
 const addAttributes = (element, attributes) =>
-  attributes.forEach(([key, value]) => {
+  Object.entries(attributes).forEach(([key, value]) => {
     element.setAttribute(key, value);
   });
 
-const createCards = (fragments) =>
-  fragments.map(({ tag, attributes, contents }) => {
-    const element = document.createElement(tag);
-    addAttributes(element, attributes);
+const createCards = ([tag, attributes, ...contents]) => {
+  const element = document.createElement(tag);
+  addAttributes(element, attributes);
 
-    if (Array.isArray(contents)) {
-      const nestedChildren = createCards(contents);
-      element.append(...nestedChildren);
-      return element;
-    }
-
-    element.textContent = contents;
+  if (contents.length === 1 && !Array.isArray(contents[0])) {
+    element.textContent = capitalize(contents.toString());
     return element;
-  });
+  }
+
+  const nestedChildren = contents.map((c) => createCards(c));
+  element.append(...nestedChildren);
+
+  return element;
+};
 
 export const renderPokemonCards = (container, pokemon) => {
   const fragments = createPokemonCardTemplates(pokemon);
-  const cards = createCards(fragments);
+  const cards = fragments.map((fragment) => createCards(fragment));
   container.replaceChildren(...cards);
 };
 
